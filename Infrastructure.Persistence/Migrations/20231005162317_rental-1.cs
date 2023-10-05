7fe5e55b54dbe57cc349a1869bab15ae9ce968f5
piotrokrutniak <91792866+piotrokrutniak@users.noreply.github.com>
2023-10-05 18:36:24 +0200

    Updated migration file

---
+++ Infrastructure.Persistence/Migrations/20231005162317_rental-1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class rental1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_IdentityUser_UserId1",
+                table: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "IdentityUser");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservations_UserId1",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Reservations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Active",
+                table: "Locations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Active",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Locations");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Reservations",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "Reservations",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "IdentityUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityUser", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_UserId1",
+                table: "Reservations",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_IdentityUser_UserId1",
+                table: "Reservations",
+                column: "UserId1",
+                principalTable: "IdentityUser",
+                principalColumn: "Id");
+        }
+    }
+}
